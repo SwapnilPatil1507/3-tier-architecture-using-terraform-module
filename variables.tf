43ba8a2c07367428bb6c169922169429242f493b
Swapnil Vilas Patil <swapnilpatilmugali@gmail.com>
2025-02-11 18:25:51 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,14 @@
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+}
+variable "access" {
+  type = string
+}
+variable "secret" {
+  type = string
+}
 variable "ami" {
-    type = string
-}
-variable "ins_type" {
- type = string
- default = "t2.micro"
-}
-variable "key_name" {
- type = string
-}
-variable "vpc_id" {
   type = string
 }
-variable "app_subnet_id" {
-  type = string
-}
+
